43ee4d7c5709525e2ae2ecc8c02989d9c3ec9924
Carrillo Ortega <acarrilloort@dxc.com>
2019-08-22 16:42:02 -0500

    Agregando axios y funcionalidad de log Operacional

--- soapclient.js
+++ soapclient.js
@@ -4,13 +4,13 @@ const Http = require('./handlers/client/http.js').HttpClientHandler
 const X509BinarySecurityToken = require('./handlers/client/security/security.js').X509BinarySecurityToken
 const Signature = require('./handlers/client/security/signature.js').Signature
 const Security = require('./handlers/client/security/security.js').SecurityClientHandler
-
+const axios = require('axios')
 
 class SoapClient {
     // ..and an (optional) custom class constructor. If one is
     // not supplied, a default constructor is used instead:
     // constructor() { }
-    constructor(certificadoSOAP) {
+    constructor(certificadoSOAP,options) {
  
         this.certificate ={
             private: fs.readFileSync(certificadoSOAP.privKey).toString(),
@@ -26,11 +26,15 @@ class SoapClient {
         this.http = new Http()
         this.security = new Security({}, [x509, signature])
         this.handlers = [this.security, this.http]
+        this.options=options
 
     }
     // Si requiere MTOM 
     setHandlers(handlers) {
       this.handlers = handlers
+    }
+    setHandlers(request) {
+      this.request = request
     }
     getHttp(){
         return this.http
@@ -40,7 +44,7 @@ class SoapClient {
         return this.security
     }
  
-    call (ctx, logger) 
+    call (ctx,pCtx, logger) 
       {
       new Promise((resolve, reject) => {
         const newctx = {
@@ -48,23 +52,103 @@ class SoapClient {
           contentType: 'text/xml',
           cert: this.certificate.public,
           key: this.certificate.private,
-        }  
+        } 
+        const shouldLog = this.options.log
+        const logOperation = this.log
+        const debug = this.options.debug
+        const opData = {
+          date: new Date().toISOString(),
+          status: 'OK',
+        } 
         send(this.handlers, newctx, (resctx) => {
-          if (logger){  
-              logger.debug(
-              `Biometria-Service:AutenticacionDactilar:SendAC > Response: ${JSON.stringify(
-                  resctx.response
-              )}`
-              
-              )
-          }
+
           if (resctx.statusCode === 200) {
-            resolve(resctx.response)
+            if (debug) {
+              if (logger){
+                logger.debug(
+                  `ClienteSoap:call:send:${
+                    opData.status
+                  }: Ctx=${JSON.stringify(
+                    pCtx
+                  )} - Request=${this.request?this.request:resctx.request} - Response=${resctx.response}`
+                )
+             }
+            }
+            if (shouldLog) {
+              logOperation(pCtx, logger, opData, this.request?this.request:resctx.request, resctx.response).then(() => {
+                resolve(resctx.response)
+              })
+            } else {
+              resolve(resctx.response)
+            }
           } else {
-            reject(new Error(resctx.response))
+            opData.status='ERROR'
+            if (debug) {
+              if (logger){
+                logger.debug(
+                  `ClienteSoap:call:send:${
+                    opData.status
+                  }: Ctx=${JSON.stringify(
+                    pCtx
+                  )} - Request=${this.request?this.request:resctx.request} - Response=${resctx.response}`
+                )
+             }
+            }
+            if (shouldLog) {
+              logOperation(pCtx, logger, opData, this.request?this.request:resctx.request, resctx.response).then(() => {
+                reject(new Error(resctx.response))
+              })
+            } else {
+              reject(new Error(resctx.response))
+            }
           }
         })
       })
+    }
+    async log (
+      ctx,
+      logger,
+      op,
+      req,
+      res
+    ){
+      const logRequest = {
+        consumer: {
+          appConsumer: {
+            id: ctx.appConsumer.id,
+            sessionId: ctx.appConsumer.sessionId,
+            transactionId: ctx.appConsumer.transaccionId,
+          },
+          deviceConsumer: {
+            id: ctx.deviceConsumer.id,
+            ip: ctx.ipAddress,
+            locale: ctx.deviceConsumer.locale,
+            terminalId: ctx.appConsumer.terminalId,
+            userAgent: ctx.deviceConsumer.userAgent,
+          },
+        },
+        documento: {
+          numero: ctx.documento.numero,
+          tipo: ctx.documento.tipo,
+        },
+        messages: {
+          idService: ctx.serviceName,
+          requestService: req,
+          responseService: res || 'SIN-DATOS',
+        },
+        operation: {
+          operationDate: op.date,
+          statusResponse: {
+            status: op.status,
+          },
+          type: ctx.type,
+        },
+      }
+      try {
+        await axios.post(this.loggerEndpoint, logRequest)
+      } catch (error) {
+        logger.error(`ClienteSoap:call:logger error: ${error.message}`)
+      }
     }
   }
 exports.SoapClient=SoapClient
